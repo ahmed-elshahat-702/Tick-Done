c16de197b8f57feb31fdbda4cb88d6672d146539
Ahmed Elshahat <ahmedelshahat702@gmail.com>
2025-07-06 20:59:52 +0300

    make logo referring to home page

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-2">
+    <Link href="/" className="flex items-center gap-2">
       <Image src="/logo.svg" alt="Logo" width={30} height={30} />
       <h1 className="font-semibold text-lg">Tick Done</h1>
-    </div>
+    </Link>
   );
 };
 
