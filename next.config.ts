0b23f2411d9999d9a1b8ad50a3caedf29637defc
Ahmed Elshahat <ahmedelshahat702@gmail.com>
2025-07-08 20:47:05 +0300

    undo pomodoro until finish

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,18 @@
-import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === "development";
-
-const baseConfig: NextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
       },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
     ],
   },
 };
 
-const withPWAWrapped = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: isDev,
-
-  importScripts: ["/custom-sw-events.js"],
-
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "google-fonts-webfonts",
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60,
-        },
-      },
-    },
-  ],
-});
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default withPWAWrapped(baseConfig as any);
+export default nextConfig;
