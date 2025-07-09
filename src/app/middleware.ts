16e172f0e51386085915fb49e1a93a4302f6c5c9
Ahmed Elshahat <ahmedelshahat702@gmail.com>
2025-07-09 08:15:01 +0300

    change matcher in middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,6 +1,5 @@
 export { auth as middleware } from "./auth";
 
 export const config = {
-  // Adjust the matcher to protect all routes except auth and public ones
-  matcher: ["/((?!api/auth|_next/static|favicon.ico).*)"],
+  matcher: ["/((?!auth/signin|auth/signup|auth/error).*)"],
 };
